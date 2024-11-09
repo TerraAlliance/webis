f7bcc091253bcdb21c9a49bc8ed15af6fb357073
Santista <terra_alliance@proton.me>
2024-11-09 15:08:10 -0300

    Removed unused function from Webis Component

--- src/Webis/Webis.jsx
+++ src/Webis/Webis.jsx
@@ -1,5 +1,3 @@
-import { Html } from "@react-three/drei"
-
 import { FlexBox } from "../ui/FlexBox"
 import { CreateBar } from "./CreateBar"
 import { StyleBar } from "./StyleBar"
@@ -16,27 +14,5 @@ export function Webis(props) {
         <StyleBar grow={1} />
       </FlexBox>
     </FlexBox>
-  )
-}
-
-export function HtmlDisplay({ x = 0, y = 0, z = 0, width, height, children, color }) {
-  return (
-    <Html
-      position={[x, y, z]}
-      distanceFactor={400}
-      transform={true}
-      occlude={"blending"}
-      style={{
-        width: width + "px",
-        height: height + "px",
-        userSelect: "none",
-        overflowY: "auto",
-        borderRadius: "10px",
-        backgroundColor: color,
-        scrollbarGutter: "stable both-edges",
-      }}
-    >
-      {children}
-    </Html>
   )
 }
